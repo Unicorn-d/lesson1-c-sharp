ff84e0c24b1f403f2221d6b53d742cf5e996deff
Dashok <dshbnv@mail.ru>
2023-10-19 03:16:40 +0300

    hheelloo

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Представтесь, пожалуйста ☺️");
+string name = Console.ReadLine();
+
+if (name.ToLower() == "даша") {
+    Console.WriteLine($"Привет, {name}! Рад тебя видеть снова!");
+} else {
+    Console.WriteLine($"Даров, {name}");
+}
