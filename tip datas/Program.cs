1d68f914e650652050573582be0615c3628b22a4
Dashok <dshbnv@mail.ru>
2023-10-19 22:29:17 +0300

    красиво и правильно

--- tip datas/Program.cs
+++ tip datas/Program.cs
@@ -1,12 +1,60 @@
-﻿int numbA = new Random().Next(0, 20);
-int numbB = new Random().Next(0, 20);
+﻿int numbA = new Random().Next(-20, 20);
+int numbB = new Random().Next(-20, 20);
 int sum = numbA + numbB;
 
-Console.WriteLine($"{numbA}+{numbB}={sum}");
+if (numbB < 0){
+    Console.WriteLine($"{numbA}+({numbB})={sum}");
+}
+else{
+    Console.WriteLine($"{numbA}+{numbB}={sum}");
+}
 
+double numbX = new Random().Next(-20, 20);
+double numbY = new Random().Next(-20, 20);
+double division = numbX/numbY;
 
-double numbX = new Random().Next(1, 20);
-double numbY = new Random().Next(1, 20);
-double division = numbX / numbY;
+if (numbX !=0){
+   division = numbX / numbY;
+}
+else{
+    numbX++;
+    division = numbX++ / numbY;
+}
 
-Console.WriteLine($"{numbX}:{numbY}={division}");
+if (numbY !=0){
+   division = numbX / numbY;
+}
+else{
+    numbY++;
+    division = numbX++ / numbY;
+}
+
+if (numbB < 0){
+    Console.WriteLine($"{numbX}:({numbY})={division}");
+}
+else{
+    Console.WriteLine($"{numbX}:{numbY}={division}");
+}
+
+int numbC = new Random().Next(-20, 20);
+int numbD = new Random().Next(-20, 20);
+int product = numbC * numbD;
+
+if (numbB < 0){
+    Console.WriteLine($"{numbC}*({numbD})={product}");
+}
+else{
+    Console.WriteLine($"{numbC}*{numbD}={product}");
+}
+
+int numbZ = new Random().Next(-20, 20);
+int numbE = new Random().Next(-20, 20);
+int difference = numbZ - numbE;
+
+if (numbB < 0){
+    Console.WriteLine($"{numbZ}-({numbE})={difference}");
+}
+else{
+    Console.WriteLine($"{numbZ}-{numbE}={difference}");
+}
+
