25a45add0900f2e37d3178bcbd7c94aac0973899
Dashok <dshbnv@mail.ru>
2023-10-19 03:42:13 +0300

    delaem krasivo

--- int and double/Program.cs
+++ int and double/Program.cs
@@ -1,9 +1,17 @@
-﻿int numbA = 5;
-int numbB = 7;
+﻿int numbA = new Random().Next(0, 20);
+
+Console.WriteLine($"{numbA} + {numbB}={}");
+
+int numbB = new Random().Next(0, 20);
+Console.WriteLine(numbB);
+
 
 Console.WriteLine(numbA + numbB);
 
-double numbX = 15;
-double numbY = 7;
+double numbX = new Random().Next(0, 20);
+Console.WriteLine(numbX);
+
+double numbY = new Random().Next(0, 20);
+Console.WriteLine(numbY);
 
 Console.WriteLine(numbX / numbY);
